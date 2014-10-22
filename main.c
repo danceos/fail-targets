d1edb4fa3a5f8c2a62532a908d2e9c3da7083ce1
Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
2014-10-22 11:23:15 +0200

    Initial commit: Simple Injection Target

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdint.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+
+volatile int __dummy;
+void __attribute__ ((noinline)) fail_marker();
+void __attribute__ ((noinline)) fail_marker()
+{
+    __dummy = 100;
+}
+
+void __attribute__ ((noinline)) stop_trace();
+void __attribute__ ((noinline)) stop_trace()
+{
+    __dummy = 100;
+}
+
+
+int array[] = {1, 1, 2, 3, 5, 8, 13, 21};
+void os_main() {
+	int sum = 0;
+
+	for (int i = 0; i < sizeof(array)/sizeof(*array); i++) {
+		sum = (array[i] * 23) + 1;
+	}
+
+	if (sum != 1250)
+		fail_marker();
+    stop_trace();
+}
+
